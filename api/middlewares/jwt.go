40c0a5d10bc5ed70a899c9d1761c1a5e7cd67cff
antojama <a.martinez@westorage.cl>
2024-11-06 08:36:45 -0300

    1.2 tools

--- api/middlewares/jwt.go
+++ api/middlewares/jwt.go
@@ -18,18 +18,18 @@ func JWT(secret string, claims jwt.MapClaims) gin.HandlerFunc {
 			if len(bearerToken) == 2 {
 				token, err := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
 					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-						return nil, fmt.Errorf("signin method not valid")
+						return nil, fmt.Errorf("el método de inicio de sesión no es válido")
 					}
 					return []byte(secret), nil
 				})
 				if _, ok := token.Claims.(jwt.MapClaims); err != nil || !ok || !token.Valid {
-					c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("invalid token: %s", err.Error())})
+					c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("token invalido : %s", err.Error())})
 					c.Abort()
 					return
 				}
 				for name, value := range claims {
 					if claim, ok := token.Claims.(jwt.MapClaims)[name]; !(ok && claim == value) {
-						c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("required claim %s not found or incorrect", name)})
+						c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("Reclamo requerido% s no encontrado o incorrecto", name)})
 						c.Abort()
 						return
 					}
@@ -38,12 +38,12 @@ func JWT(secret string, claims jwt.MapClaims) gin.HandlerFunc {
 				c.Set("decoded", token.Claims)
 				c.Next()
 			} else {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "authorization header not properly formatted, should be Bearer + {token}"})
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "El encabezado de autorización no tiene el formato correcto, debería ser Bearer + {token}"})
 				c.Abort()
 				return
 			}
 		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "an authorization header is required"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Se requiere un encabezado de autorización"})
 			c.Abort()
 			return
 		}
